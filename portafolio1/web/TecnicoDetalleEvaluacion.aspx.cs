e86ab294350f29cbc9385ae1cebf0fc418d17908
oscarDsalgado <32973939+oscarDsalgado@users.noreply.github.com>
2017-10-25 12:20:42 -0300

    Listar y ver detalle de evaluacion
    Se listan las evaluaciones por tecnico y los detalles de estas.

---
+++ portafolio1/web/TecnicoDetalleEvaluacion.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web
+{
+    public partial class TecnicoDetalleEvaluacion : System.Web.UI.Page
+    {
+        string rut = " ";
+        int id = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["tipo"].ToString() != "Tecnico")
+            {
+                Response.Redirect("Home.aspx");
+            }
+
+            rut = Session["Rut"].ToString();
+            id = int.Parse(Session["Id_evaluacion"].ToString());
+
+            if (!IsPostBack)
+            {
+                foreach (var item in BLL.EvaluacionDTO.ListarEvaluacionTecnico(rut))
+                {
+
+
+                    if (item.Id == id)
+                    {
+                        string nombre = BLL.EmpresaDTO.NombreEpresa(item.Id_empresa);
+                        string nombreE = BLL.TipoEvaluacionDTO.nombreEvaluaciones(item.Id_tipo);
+                        Empresa.Text = nombre;
+                        lbl_Rut.Text = item.Id_empresa;
+                        lbl_tipo.Text = nombreE;
+                        lblfecha.Text = item.Fecha.ToString().Substring(0,11);
+
+                        GridView1.DataSource = GetData(item.Id_tipo, item.Id);
+                        GridView1.DataBind();
+
+                    }
+
+                    
+
+
+
+                }
+
+            }
+
+
+        }
+
+
+        public DataTable GetData(int id, int idE)
+        {
+            DataTable dt = new DataTable();
+
+
+            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Pregunta"), new DataColumn("Respuesta") });
+
+            foreach (var item in BLL.DetallePreguntaDTO.ListarDetalle(idE))
+            {
+
+
+                foreach (var item2 in BLL.CategoriaDTO.ListarXtipo(id))
+                {
+                    foreach (var item3 in BLL.PreguntasDTO.ListarXcategoria(item2.Id))
+                    {
+
+                        if (item.Id_pregunta == item3.Id)
+                        {
+                            dt.Rows.Add(item.Id_pregunta,item3.Pregunta1,item.Pregunta);
+                        }
+                    }
+                }
+
+                
+
+               // dt.Rows.Add(item.Id_pregunta,);
+
+            }
+
+            return dt;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("tecnicoListEv.aspx");
+        }
+    }
+}
---
+++ portafolio1/web/TecnicoDetalleEvaluacion.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace web {
+    
+    
+    public partial class TecnicoDetalleEvaluacion {
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control Empresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Empresa;
+        
+        /// <summary>
+        /// Control lbl_Rut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_Rut;
+        
+        /// <summary>
+        /// Control lbl_tipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_tipo;
+        
+        /// <summary>
+        /// Control lblfecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblfecha;
+        
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
